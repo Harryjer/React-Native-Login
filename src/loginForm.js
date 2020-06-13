7824c60ba4ed8640a88c5e820bfbc3e6f0a446a7
Thisal Kodithuwakku <33801630+Harryjer@users.noreply.github.com>
2020-06-13 23:35:25 +0530

    Add files via upload

---
+++ src/loginForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+class LoginForm extends React.Component {
+    render() {
+        return (
+            <div className="loginForm">
+                Thisal
+            </div>
+        );
+    }
+}
+export default LoginForm;
---
+++ src/stores/UserStore.js
@@ -0,0 +1,11 @@
+import { extendObservable } from 'mobx';
+class UserStore {
+    constructor() {
+        extendObservable(this, {
+            loading: true,
+            isLoggedIn: false,
+            username: ""
+        })
+    }
+}
+export default new UserStore();
